61144939ff20ab9f7d4528d22d339244be999402
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-18 18:40:42 +0200

    release scripts

--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "os/exec"
+	"fmt"
+	"os"
+	"os/exec"
 
-  "github.com/fatih/color"
+	"github.com/fatih/color"
 )
 
-func main () {
-  args := os.Args
+func main() {
+	args := os.Args
 
-  if len(args)<2 {
-    fmt.Fprintln(os.Stderr, "Missing arguments")
-    os.Exit(1)
-  }
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "Missing arguments")
+		os.Exit(1)
+	}
 
-  bin := args[1]
-  cmdArgs := make([]string, 0)
-  if len(args)>1 {
-    cmdArgs = args[2:]
-  }
+	bin := args[1]
+	cmdArgs := make([]string, 0)
+	if len(args) > 1 {
+		cmdArgs = args[2:]
+	}
 
-  oCmd := exec.Command(bin, cmdArgs...)
+	oCmd := exec.Command(bin, cmdArgs...)
 	oCmd.Stdout = os.Stdout
 	oCmd.Stderr = os.Stderr
-  err := oCmd.Run()
+	err := oCmd.Run()
 
-  if err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    fmt.Fprintln(os.Stderr, color.RedString(" ✘ Failed"))
-    os.Exit(1)
-  }
-  fmt.Println(color.GreenString(" ✔ Success"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, color.RedString(" ✘ Failed"))
+		os.Exit(1)
+	}
+	fmt.Println(color.GreenString(" ✔ Success"))
 
-  }
+}
